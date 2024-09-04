0f232776666103b433c92092905b7e004bd17c9c
manu006m <manu006m@gmail.com>
2024-09-04 16:05:08 +0530

    manu

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ const router=express.Router()
 const student=require("./student/studentController")
 const tutor = require("./tutor/tutorController")
 const books = require("./books/booksController")
-const rent = require("./rendedBooks/rendedBooksController")
+const rentBookByTutor = require("./rendedBooks/rendedBooksController")
+
 
 router.post("/student-signUp",student.upload,student.addStudent)
 
@@ -14,16 +15,24 @@ router.post("/tutorSignup",tutor.upload,tutor.addTutor)
 router.post("/tutorLogin",tutor.tutoLogin)
 router.get("/tutorViewProfile/:id",tutor.viewProfile)
 router.post("/tutor-forgot-password",tutor.TutorForgetPassword)
+router.get("/view-all/tutor",tutor.viewAllTutors) 
+router.put("/approveTutor/:id",tutor.ApproveTutorsById)
+router.put("/rejectTutor/:id",tutor.rejectTutorsById)
+router.get("/viewAllApprovedTutors",tutor.viewAllApprovedTutors)
+router.get("/viewAllPendingTutors",tutor.viewAllPendingTutors)
+router.get("/viewAllRejectedTutors",tutor.viewAllRejectedTutors)
+router.post("/rendBookByTutor" ,rentBookByTutor.addRentBook) 
 
 
-// books
+// booksaddRentBook
 
 router.post("/add-books",books.upload,books.addBooks)
 router.get("/viewAllBooks",books.viewAllBook)
 router.get("/view-single-product/:id",books.tutorViewSingleProduct)
 
 // rent
+router.post("/rendBookByTutor" ,rentBookByTutor.addRentBook) 
+router.post("/tutorViewRental",rentBookByTutor.tutorViewRental)
 
-router.post("/rentBooks",rent.addRentBook)
 
 module.exports=router
